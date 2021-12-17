9b5f1cfb4a924b14895642a5a762f5b9fa84ff0a
Jérémy Boehm <jeremyboehm77@gmail.com>
2021-12-17 19:07:03 +0100

    Refactoring

--- src/main/java/kata/bank/Account.java
+++ src/main/java/kata/bank/Account.java
@@ -2,14 +2,7 @@ package kata.bank;
 
 public class Account {
 
-    public static int deposit(int balance, int amount) {
-        if (amount == 2) {
-            return 2;
-        }
-        if (amount == 1) {
-            return 1;
-        }
-
-        return 0;
+    public static int deposit(int amount) {
+        return amount;
     }
 }
--- src/test/java/kata/bank/DepositTest.java
+++ src/test/java/kata/bank/DepositTest.java
@@ -6,34 +6,31 @@ import org.junit.Test;
 public class DepositTest {
 
     @Test
-    public void should_increase_the_account_balance_when_a_deposit_is_made() {
+    public void should_increase_the_account_balance_to_0_when_the_deposit_is_0() {
 
-        int balance = 0;
         int amount = 0;
 
-        int updatedBalance = Account.deposit(balance, amount);
+        int updatedBalance = Account.deposit(amount);
 
         Assertions.assertThat(updatedBalance).isEqualTo(0);
     }
 
     @Test
-    public void should_increase_the_account_balance_when_a_deposit_is_made_2() {
+    public void should_increase_the_account_balance_to_1_when_the_deposit_is_1() {
 
-        int balance = 0;
         int amount = 1;
 
-        int updatedBalance = Account.deposit(balance, amount);
+        int updatedBalance = Account.deposit(amount);
 
         Assertions.assertThat(updatedBalance).isEqualTo(1);
     }
 
     @Test
-    public void should_increase_the_account_balance_when_a_deposit_is_made_3() {
+    public void should_increase_the_account_balance_to_2_when_the_deposit_is_2() {
 
-        int balance = 0;
         int amount = 2;
 
-        int updatedBalance = Account.deposit(balance, amount);
+        int updatedBalance = Account.deposit(amount);
 
         Assertions.assertThat(updatedBalance).isEqualTo(2);
     }
