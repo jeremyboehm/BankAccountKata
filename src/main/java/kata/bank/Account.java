787633894a3999121302d502666de9a3912aff93
Jérémy Boehm <jeremyboehm77@gmail.com>
2021-12-17 18:48:59 +0100

    With balance 0 and deposit amount 0 then updated balance is 0

---
+++ src/main/java/kata/bank/Account.java
@@ -0,0 +1,8 @@
+package kata.bank;
+
+public class Account {
+
+    public static int deposit(int balance, int amount) {
+        return 0;
+    }
+}
---
+++ src/test/java/kata/bank/DepositTest.java
@@ -0,0 +1,18 @@
+package kata.bank;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class DepositTest {
+
+    @Test
+    public void should_increase_the_account_balance_when_a_deposit_is_made() {
+
+        int balance = 0;
+        int amount = 0;
+
+        int updatedBalance = Account.deposit(balance, amount);
+
+        Assertions.assertThat(updatedBalance).isEqualTo(0);
+    }
+}
