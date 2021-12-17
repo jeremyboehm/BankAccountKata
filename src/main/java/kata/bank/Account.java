0265e683a7f3ba5b7151021777e36595e337c431
Jérémy Boehm <jeremyboehm77@gmail.com>
2021-12-17 18:57:57 +0100

    With balance 0 and deposit amount 1 then updated balance is 1

--- src/main/java/kata/bank/Account.java
+++ src/main/java/kata/bank/Account.java
@@ -3,6 +3,10 @@ package kata.bank;
 public class Account {
 
     public static int deposit(int balance, int amount) {
+        if (amount == 1) {
+            return 1;
+        }
+
         return 0;
     }
 }
--- src/test/java/kata/bank/DepositTest.java
+++ src/test/java/kata/bank/DepositTest.java
@@ -15,4 +15,15 @@ public class DepositTest {
 
         Assertions.assertThat(updatedBalance).isEqualTo(0);
     }
+
+    @Test
+    public void should_increase_the_account_balance_when_a_deposit_is_made_2() {
+
+        int balance = 0;
+        int amount = 1;
+
+        int updatedBalance = Account.deposit(balance, amount);
+
+        Assertions.assertThat(updatedBalance).isEqualTo(1);
+    }
 }
