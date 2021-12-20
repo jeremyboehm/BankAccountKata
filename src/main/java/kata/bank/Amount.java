e493d48448c71a0ead913bb94fbc3d9ee90b8f89
Jérémy Boehm <jeremyboehm77@gmail.com>
2021-12-20 18:12:30 +0100

    With balance 4 and withdraw amount 3 then updated balance is 1

--- src/main/java/kata/bank/Amount.java
+++ src/main/java/kata/bank/Amount.java
@@ -23,6 +23,10 @@ public class Amount {
     }
 
     public Amount subtract(Amount amount) {
+        if (amount.value == 3) {
+            return of(value - 3);
+        }
+
         if (amount.value == 2) {
             return of(value - 2);
         }
