f56f190c61291442180dc09b9f9bdd8e6959d9e0
Jérémy Boehm <jeremyboehm77@gmail.com>
2021-12-22 18:12:03 +0100

    Add Account test coverage

---
+++ src/test/java/kata/bank/AccountTest.java
@@ -0,0 +1,27 @@
+package kata.bank;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class AccountTest {
+
+    @Test
+    public void should_increase_balance_when_deposit() {
+        Account account = Account.of(Amount.of(500));
+        Amount givenAmount = Amount.of(100);
+
+        Amount result = account.deposit(givenAmount);
+
+        Assertions.assertThat(result).isEqualTo(Amount.of(600));
+    }
+
+    @Test
+    public void should_decrease_balance_when_withdraw() {
+        Account account = Account.of(Amount.of(500));
+        Amount givenAmount = Amount.of(100);
+
+        Amount result = account.withdraw(givenAmount);
+
+        Assertions.assertThat(result).isEqualTo(Amount.of(400));
+    }
+}
